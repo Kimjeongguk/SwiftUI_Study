fbda652cf8a1accd8b9f6e48d73cbae3a63bc998
KimJeongguk <mindstar7949@naver.com>
2024-01-05 22:38:13 +0900

    RPSGame 기능구현

--- RPSGame/RPSGame/ContentView.swift
+++ RPSGame/RPSGame/ContentView.swift
@@ -7,37 +7,130 @@
 
 import SwiftUI
 //🖐️✌️✊
+enum ResultGame {
+    case win
+    case lose
+    case draws
+}
+
+struct CustomButton: ViewModifier {
+    var title: String
+    var void: () -> Void
+    func body(content: Content) -> some View {
+        Button(title, action: void)
+            .font(.largeTitle)
+            .frame(width: 300, height: 100)
+            .foregroundStyle(.white)
+            .fontWeight(.bold)
+            .background(.blue)
+            .clipShape(.capsule)
+            .padding()
+    }
+}
+
+extension View {
+    func clickButton(title:String, void: @escaping () -> Void) -> some View {
+        modifier(CustomButton(title: title, void: void))
+    }
+}
+
 struct ContentView: View {
+    @State var score = 0
+    @State var round = 0
     @State var app = ""
-    @State var user = "rock"
-    
-    private var appArray = ["rock", "paper", "scissors"].shuffled()
+    @State var user = ResultGame.win
+    @State var showingScore = false
+    @State var reset = false
+    @State var correctAnswer = Int.random(in: 0...2)
+    @State private var appArray = ["rock", "paper", "scissors"].shuffled()
     private var userArray = ["rock", "paper", "scissors"]
-    computer = computer
+    
     var body: some View {
         
         VStack {
             Section("선택"){
-                ForEach(0..<3) {
-                    Button(userArray[$0]) {
-                        print("")
+                ForEach(0..<3) { index in
+                    clickButton(title: userArray[index]){
+                        resultOfGame(userSelet: userArray[index])
                     }
-                    .font(.largeTitle)
-                    .frame(width: 300, height: 100)
-                    .foregroundStyle(.white)
-                    .fontWeight(.bold)
-                    .background(.blue)
-                    .clipShape(.capsule)
-                    .padding()
                 }
             }
         }
         .padding()
+        .alert("SCORE", isPresented: $showingScore) {
+            Button("Next Game", action: resetGame)
+        } message: {
+            switch user {
+            case .win: Text("app:\(app) 이겼다!")
+            case .draws: Text("app:\(app) 비겼어")
+            case .lose: Text("app:\(app) 졌어 ㅜㅜ")
+            }
+        }
+        .alert("Totle Score", isPresented: $reset){
+            Button("처음으로", action: totalScore)
+        } message: {
+            Text("총점수: \(score)")
+        }
     }
     
-    func mmmm(user: String){
-        if user == rock
+    func appResult() {
+        app = appArray[correctAnswer]
     }
+    
+    func resultOfGame(userSelet: String) {
+        appResult()
+        if userSelet == "rock" {
+            if app == "rock" {
+                self.user = .draws
+            } else if app == "paper" {
+                self.user = .lose
+            } else if app == "scissors" {
+                self.user = .win
+            }
+        } else if userSelet == "paper" {
+            if app == "rock" {
+                self.user = .win
+            } else if app == "paper" {
+                self.user = .draws
+            } else if app == "scissors" {
+                self.user = .lose
+            }
+        } else if userSelet == "scissors" {
+            if app == "rock" {
+                self.user = .lose
+            } else if app == "paper" {
+                self.user = .win
+            } else if app == "scissors" {
+                self.user = .draws
+            }
+        }
+        round += 1
+        scoreCalculation()
+        showingScore = true
+    }
+    
+    func scoreCalculation() {
+        switch self.user {
+        case .draws: break
+        case .lose: score -= 100
+        case .win: score += 100
+        }
+    }
+    
+    func totalScore() {
+        round = 0
+        score = 0
+    }
+    
+    func resetGame() {
+        if round > 2 {
+            reset = true
+        }
+        appArray.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
+    
 }
 
 #Preview {
