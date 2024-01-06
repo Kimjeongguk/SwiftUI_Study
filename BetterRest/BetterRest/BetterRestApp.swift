3c7e2b8e61e90acab5f47cc81d9bacc5ff45cbc8
KimJeongguk <mindstar7949@naver.com>
2024-01-06 22:11:49 +0900

    BetterRest

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Jeongguk Kim on 1/6/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
