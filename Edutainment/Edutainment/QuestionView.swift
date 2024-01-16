8ed449d1c612071296ad24f6c7528e5c878e4f89
jeongguk <star7949@thinkm.co.kr>
2024-01-16 17:45:41 +0900

    ...

--- Edutainment/Edutainment/QuestionView.swift
+++ Edutainment/Edutainment/QuestionView.swift
@@ -10,9 +10,21 @@ import SwiftUI
 struct QuestionView: View {
     var level: Int
     var question: Int
+    @State private var answer = 0
+    @State private var array: [Int] = []
     
     var body: some View {
-        Text("\(level), \(question)")
+        VStack {
+            Text("\(level), \(question)")
+            TextField("Answer", value: $answer, format: .number)
+        }
+    }
+    
+    func setArray() {
+        for i in 1...question {
+            array.append(i)
+        }
+        array.shuffle()
     }
 }
 
